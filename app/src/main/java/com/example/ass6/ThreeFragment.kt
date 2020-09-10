6aefbcac33a5c08a707446d857207d9bd92ba34c
Atsadawut-cha <69954288+Atsadawut-cha@users.noreply.github.com>
2020-09-10 15:10:27 +0700

    Ass6_Sec3_613020179-2

---
+++ app/src/main/java/com/example/ass6/ThreeFragment.kt
@@ -0,0 +1,33 @@
+package com.example.ass6
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_three.view.*
+import kotlinx.android.synthetic.main.fragment_two.view.*
+
+
+class ThreeFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_three, container, false)
+        view.bntClickFragThree.setOnClickListener() {
+            var fragment: Fragment? = null
+            fragment = OneFragment()
+            replaceFragment(fragment)
+        }
+        return view
+    }
+
+    fun replaceFragment(someFragment: Fragment) {
+        val transaction = fragmentManager!!.beginTransaction()
+        transaction.replace(R.id.frameLayout, someFragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+}
